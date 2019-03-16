2803336bff9ce60475ad3fed7199b9e9df353e3f
gsarti <gabriele.sarti996@gmail.com>
2019-03-16 13:13:29 +0100

    Fixed repeated use of same auxiliary matrix

--- code/02_matrix_multiplication/homework.c
+++ code/02_matrix_multiplication/homework.c
@@ -34,7 +34,7 @@ void naive_mult(Matrix A, Matrix B, Matrix C, const size_t size)
    for (size_t i = 0; i < size; i++) {
      for (size_t j = 0; j < size; j++) {
        C.mat[C.row + i][C.col + j] = 0.0;
-       for (size_t k=0; k<size; k++) {
+       for (size_t k = 0; k < size; k++) {
          C.mat[C.row + i][C.col + j] += (A.mat[A.row + i][A.col + k] *
                                          B.mat[B.row + k][B.col + j]);
        }
@@ -69,63 +69,63 @@ void strassen_aux_opt(Matrix A, Matrix B, Matrix C, Matrix M, const size_t size)
   Matrix M22 = {M.mat, M.row + half_size, M.col + half_size};
 
   // S2 = A11 + A12
-  sum_blocks(A11, A12, C12, half_size);
+  sum_blocks(A11, A12, C22, half_size);
   // P2 = S2 x B22
-  strassen_aux_opt(C12, B22, C12, M22, half_size);
+  strassen_aux_opt(C22, B22, C12, M22, half_size);
 
   // S4 = B21 - B11
-  sub_blocks(B21, B11, C21, half_size);
+  sub_blocks(B21, B11, C22, half_size);
   // P4 = A22 x S4
-  strassen_aux_opt(A22, C21, C21, M22, half_size);
+  strassen_aux_opt(A22, C22, M21, M22, half_size);
 
   // S5 = A11 + A22
   sum_blocks(A11, A22, C22, half_size);
   // S6 = B11 + B22
-  sum_blocks(B11, B12, M11, half_size);
+  sum_blocks(B11, B12, C21, half_size);
   // P5 = S5 x S6
-  strassen_aux_opt(C22, M11, C22, M22, half_size);
+  strassen_aux_opt(C22, C21, M12, M22, half_size);
 
   // S7 = A12 - A22
-  sub_blocks(A12, A22, C11, half_size);
+  sub_blocks(A12, A22, C22, half_size);
   // S8 = B21 + B22
-  sum_blocks(B21, B22, M11, half_size);
+  sum_blocks(B21, B22, C12, half_size);
   // P6 = S7 x S8
-  strassen_aux_opt(C11, M11, M11, M22, half_size);
+  strassen_aux_opt(C22, C12, M11, M22, half_size);
 
   // C11 = (P5 + P4) - P2 + P6
-  sum_blocks(C22, C21, C11, half_size);
-  sum_blocks(C12, M11, M11, half_size);
-  sub_blocks(C11, M11, C11, half_size);
+  sum_blocks(M12, M21, C11, half_size);
+  sub_blocks(C11, C12, C11, half_size);
+  sum_blocks(C11, M11, C11, half_size);
 
   // S1 = B12 - B22
-  sub_blocks(B12, B22, M11, half_size);
+  sub_blocks(B12, B22, C21, half_size);
   // P1 = A11 x S1
-  strassen_aux_opt(A11, M11, M11, M22, half_size);
+  strassen_aux_opt(A11, C21, C22, M22, half_size);
 
   // C12 = P1 + P2
-  sum_blocks(M11, C12, C12, half_size);
+  sum_blocks(C22, C12, C12, half_size);
 
   // S3 = A21 + A22
-  sum_blocks(A21, A22, M12, half_size);
+  sum_blocks(A21, A22, C21, half_size);
   // P3 = S3 x B11
-  strassen_aux_opt(M12, B11, M12, M22, half_size);
+  strassen_aux_opt(C21, B11, M11, M22, half_size);
 
   // C21 = P3 + P4
-  sum_blocks(M12, C21, C21, half_size);
+  sum_blocks(M11, M21, C21, half_size);
 
   // Partial = P5 + P1 - P3
-  sum_blocks(C22, M11, C22, half_size);
-  sub_blocks(C22, M12, C22, half_size);
+  sum_blocks(M12, C22, C22, half_size);
+  sub_blocks(C22, M11, C22, half_size);
 
   // S9 = A11 - A21
   sub_blocks(A11, A21, M11, half_size);
   // S10 = B11 + B12
   sum_blocks(B11, B12, M12, half_size);
-  // P7 = S9 x S10 (replaces P2 in tmp_prod2)
-  strassen_aux_opt(M11, M12, M11, M22, half_size);
+  // P7 = S9 x S10 
+  strassen_aux_opt(M11, M12, M21, M22, half_size);
 
   // C22 = Partial - P7
-  sub_blocks(C22, M11, C22, half_size);
+  sub_blocks(C22, M21, C22, half_size);
 }
 
 void strassen_opt(float **C, float **A, float **B, const size_t size)
